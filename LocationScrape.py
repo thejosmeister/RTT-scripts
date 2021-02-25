7f43567844fd3db8767d9fb32392a619fa7d5292
JosiahACT <josiah.filleul@weareact.com>
2021-02-25 18:14:26 +0000

    New locations

--- LocationScrape.py
+++ LocationScrape.py
@@ -4,8 +4,8 @@ import csv
 import sys
 
 # pageurl = sys.argv[1]
-pageurl = 'https://www.realtimetrains.co.uk/train/03483/2021-02-20/detailed'
-filename = '5Z74_locations'
+pageurl = 'https://www.realtimetrains.co.uk/train/U36764/2021-02-20/detailed'
+# filename = 'test_locations'
 # The page we want to find the list of services for a station
 page = requests.get(pageurl)
 
@@ -21,18 +21,57 @@ dicts_of_locations = []
 for a in locations.find_all('div', class_='location'):
     if a.find('a', {'class': 'name'}) is not None and a.find('div', {'class': 'wtt'}) is not None:
         location = {'location': a.find('a', {'class': 'name'}).text}
+
+        # Arrival Times
         if a.find('div', {'class': 'wtt'}).find('div', {'class': 'arr'}) is not None:
             arrTime = a.find('div', {'class': 'wtt'}).find('div', {'class': 'arr'}).text
             if len(arrTime) == 4:
                 location['arr'] = arrTime
             elif len(arrTime) == 5:
                 location['arr'] = arrTime[:4] + '.5'
+
+        # Departure Times
         if a.find('div', {'class': 'wtt'}).find('div', {'class': 'dep'}) is not None:
             depTime = a.find('div', {'class': 'wtt'}).find('div', {'class': 'dep'}).text
             if len(depTime) == 4:
                 location['dep'] = depTime
             elif len(depTime) == 5:
                 location['dep'] = depTime[:4] + '.5'
+
+        # Platform
+        if a.find('div',{'class': 'platform'}) is not None:
+            platform = a.find('div',{'class': 'platform'}).text
+            if len(platform) > 0:
+                location['plat'] = platform
+
+        # Route
+        if a.find('div', {'class': 'route'}) is not None:
+
+            # Path
+            if a.find('div', {'class': 'route'}).find('div', {'class': 'path act c'}) is not None:
+                location['path'] = a.find('div', {'class': 'route'}).find('div', {'class': 'path act c'}).text
+            if a.find('div', {'class': 'route'}).find('div', {'class': 'path exp c'}) is not None:
+                location['path'] = a.find('div', {'class': 'route'}).find('div', {'class': 'path exp c'}).text
+
+            # Line
+            if a.find('div', {'class': 'route'}).find('div', {'class': 'line act c'}) is not None:
+                location['line'] = a.find('div', {'class': 'route'}).find('div', {'class': 'line act c'}).text
+            if a.find('div', {'class': 'route'}).find('div', {'class': 'line exp c'}) is not None:
+                location['line'] = a.find('div', {'class': 'route'}).find('div', {'class': 'line exp c'}).text
+
+        # Allowances
+        addl = a.find('div', {'class': 'addl'})
+        if addl is not None:
+            allowance = addl.find('span', {'class': 'allowance'})
+            if allowance is not None:
+                if allowance.find('span', {'class': 'eng'}) is not None:
+                    location['eng allow'] = allowance.find('span', {'class': 'eng'}).text
+                if allowance.find('span', {'class': 'pth'}) is not None:
+                    location['pth allow'] = allowance.find('span', {'class': 'pth'}).text
+                if allowance.find('span', {'class': 'prf'}) is not None:
+                    location['prf allow'] = allowance.find('span', {'class': 'prf'}).text
+
+
         dicts_of_locations.append(location)
 
 import pprint
@@ -41,6 +80,8 @@ pp = pprint.PrettyPrinter(indent=4)
 
 pp.pprint(dicts_of_locations)
 
+header = soup.find('div', class_='header')
+filename = header.text[:4] + '_locations'
 # with open('1F05_locations.csv', mode='w') as location_file:
 #     fieldnames = ['location', 'arr', 'dep']
 #     writer = csv.DictWriter(location_file, fieldnames=fieldnames)
